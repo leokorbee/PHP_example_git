739ba89867c9838e97a0d55b6440d229e98e4197
Leo Korbee <13188612+leokorbee@users.noreply.github.com>
2022-02-09 07:07:47 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+  
+<?php
+  
+  
+  
+  
+  
+  <h1>
+    writeMsg(); // call the function
+  </h1>
+  
+    
+  $colors = array("red", "green", "blue", "yellow"); 
+
+  foreach ($colors as $value) 
+  {
+    echo "$value <br>";
+  }
+    
+    
+    
+    
+    
+  function writeMsg() 
+  {
+    echo "Netwerken voor Software Developers";
+  }
+  
+    
+?>
+
+</html>
